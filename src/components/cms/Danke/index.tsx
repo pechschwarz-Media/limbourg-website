20ed7e1dfbde5b05d64626a7aa34f75cccd3905b
Dreihuhn <flo.luegering@hotmail.de>
2025-05-16 17:43:11 +0200

    Anpassung

--- src/components/cms/Danke/index.tsx
+++ src/components/cms/Danke/index.tsx
@@ -9,7 +9,7 @@ export default function Danke() {
             dataComponent="Danke"
             settings={{ padding: { top: 'large', bottom: 'large' }, variant: 'light' }}
             className="!pt-30 !pb-30">
-            <div className="container min-h-[60vh] flex flex-col items-start justify-center lg:max-w-[60%]">
+            <div className="container min-h-[60vh] flex flex-col items-start justify-center">
                 <div className="mb-12 grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-[auto_16rem]">
                     <h1 className="text-h0 leading-snug">Vielen Dank für Ihre Nachricht.</h1>
                 </div>
